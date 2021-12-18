a23abb7371c929dba5e2e6fea5b4ed04c9990214
eraser5th <hisanniwo0202@gmail.com>
2021-12-19 01:28:00 +0900

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Main from './Main';
 
-const Home: React.VFC = () => (
-  <>
-    <main>
-      <h2>Welcome to the homepage!</h2>
-      <p>You can do this, I believe in you.</p>
-    </main>
-    <nav>
-      <Link to="/about">About</Link>
-    </nav>
-  </>
-);
+const Home: React.VFC = () => {
+  const id = useRef<HTMLInputElement>(null);
+  const password = useRef<HTMLInputElement>(null);
+  const submit = (): void => {
+    fetch(`url/?id=${id.current?.value}&password=${password.current?.value}`)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <form action={`url/?id=${id.current?.value}&password=${id.current?.value}`}>
+        Name:
+        <input ref={id} type="text" name="id" />
+        Password:
+        <input ref={password} type="text" name="password" />
+        <button type="button" value="Submit" onClick={submit}>
+          submit
+        </button>
+      </form>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+      <Main />
+    </>
+  );
+};
 
 const About: React.VFC = () => (
   <>
