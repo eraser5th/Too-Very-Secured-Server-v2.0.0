b5cff0a4c3c462163acc4c718f43d0bf3220f68a
eraser5th <hisanniwo0202@gmail.com>
2021-12-28 15:46:14 +0900

    change time

--- src/Charts.tsx
+++ src/Charts.tsx
@@ -37,7 +37,7 @@ const parseData = (dataList: DataType[]): DataLists => {
   dataList
     .sort((a, b) => a.timestamp - b.timestamp)
     .forEach((data) => {
-      const time = (new Date(data.timestamp)).toTimeString();
+      const time = (new Date(data.timestamp + 9 * 60 * 60 * 1000)).toUTCString();
       tempDataList.push({ time, temperature: data.temperature });
       humidityDataList.push({ time, humidity: data.humidity });
       pressureDataList.push({ time, pressure: data.pressure });
